756d8a510fac9df0bd93b592bf815712fe0d0d55
Juiceee <lbehr@student.42.fr>
2024-07-01 17:05:17 +0200

    good

--- createContract.sh
+++ createContract.sh
@@ -2,27 +2,56 @@
 echo "Entrez le nom du fichier :"
 read fileName
 
-echo "Entrez le type de fichier (1: contract, 2: interface, 3: library):"
-read fileType
+echo "Entrez le type de fichier (1: contract, 2: interface, 3: library, 4: test):"
+read fileTypeNum
 
-if [ $fileType -eq 1 ]
+if [ $fileTypeNum -eq 1 ]
 then
-    fileType="contract"
-elif [ $fileType -eq 2 ]
+	fileType="contract"
+elif [ $fileTypeNum -eq 2 ]
 then
-    fileType="interface"
-elif [ $fileType -eq 3 ]
+	fileType="interface"
+elif [ $fileTypeNum -eq 3 ]
 then
-    fileType="library"
+	fileType="library"
+elif [ $fileTypeNum -eq 4 ]
+then
+	fileType="contract"
+	fileTest="$fileName.t.sol"
 else
-    echo "Type de fichier non reconnu."
-    exit 1;
+	echo "Type de fichier non reconnu."
+	exit 1;
 fi
 
+echo "Voulez-vous placer le fichier dans le dossier src ? (y/n)"
+read placeInSrc
+
+if [ "$placeInSrc" == "y" ]
+then
+	destination="src/$fileName.sol"
+else
+	destination="$fileName.sol"
+fi
+if [ $fileTypeNum -eq 4 ]
+then
+	echo "// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import \"forge-std/Test.sol\";
+
+contract $fileName"Test" is Test {
+	address public constant ADD_0 = 0x0000000000000000000000000000000000000000;
+	address testAdd1;
+	function setUp() public {
+	}
+}" >> test/$fileTest
+	exit
+fi
 echo "// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.20;
 
 $fileType $fileName {
 
-}" > $fileName.sol
+}" > $destination
